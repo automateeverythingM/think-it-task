40bbb71a1f3822b82d07cea0703e1d0fa6e73869
automateeverythingM <markospavicbg@gmail.com>
2023-03-01 22:31:32 +0100

    layout and top bar

---
+++ src/app/components/header/topNav/TopNav.tsx
@@ -0,0 +1,15 @@
+import styles from "./top-nav.module.css";
+import UserIcon from 'src/assets/inline-icon/user.svg';
+import { BiUser } from "react-icons/bi";
+interface ITopNavProps {}
+
+export const TopNav = ({}: ITopNavProps) => {
+  return <nav className={styles.topNav}>
+      <h1 className={styles.title}>Test</h1>
+
+      <div className={styles.userContainer}>
+        <span className={styles.userName}>Name</span>
+        <UserIcon />
+      </div>
+  </nav>;
+};
